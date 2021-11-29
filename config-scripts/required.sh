6addee10cb749f11e56ec954dbc70d38185fc6d6
The1TrueJoe <jttelaak@hotmail.com>
2021-11-29 15:11:59 -0500

    Required installs

---
+++ config-scripts/required.sh
@@ -0,0 +1,8 @@
+# Required command-line utilities for this application
+echo "Installing required command-line utilites"
+sudo apt-get install curl wget svn -y
+
+# Install JAVA
+
+# Install PERL
+sudo apt-get install perl -y
